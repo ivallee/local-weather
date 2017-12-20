468af52cba6d693a409ecaf2e51a9693eeddd180
ivallee <isaacvallee@gmail.com>
2017-12-20 09:59:44 -0800

    style and add temp unit toggle

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
 $( document ).ready(function() {
-  
+  var celsius = true;
   var weatherAPI = 'https://fcc-weather-api.glitch.me/api';
     
   function getWeather(lat, long) {
     var query = weatherAPI + '/current?lat=' + lat + '&lon=' + long;
     $.getJSON(query, function(weatherData) {
-      $('#city').html(weatherData.name);
-      $('#country').html(weatherData.sys.country);
+      $('.location').html(weatherData.name + ', ' + weatherData.sys.country);
       $('#temp').html(weatherData.main.temp.toFixed(1));
+      $('#temp-unit').html('C')
       var img = $('<img id="dynamic">');
       img.attr('src', weatherData.weather[0].icon);
       img.appendTo('.icon');
     });
+  }
+
+  $('#temp-btn').on('click', toggleTempUnit);
+
+  function toggleTempUnit(){
+    var temp = $('#temp').html();
+    if (celsius) {
+      var far = temp * 9 / 5 + 32;
+      $('#temp').html(far.toFixed(1));
+      $('#temp-unit').html('F');
+      celsius = false;
+    } else {
+      var cel = (temp - 32) * 5 / 9;
+      $('#temp').html(cel.toFixed(1));
+      $('#temp-unit').html('C');
+      celsius = true;
+    }
   }
 
   if ("geolocation" in navigator) {
